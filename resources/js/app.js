7ea01c3b23ec72fd4200cd8b87108b5994614ccf
raldokusuma <raldo0104@gmail.com>
2024-05-30 16:29:39 +0700

    feat: add vue yay

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+import App from './App.vue';
+Vue.use(VueAxios, axios);
+import IndexComponent from './components/posts/Index.vue';
+
+const routes = [
+    {
+        name: 'posts',
+        path: '/',
+        component: IndexComponent
+    }
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
+
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any','.*');
