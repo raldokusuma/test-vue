ac82ed545d1f32457244cb0a3a891c1db846a714
raldokusuma <raldo0104@gmail.com>
2024-05-31 18:19:40 +0700

    feat: show data only

--- resources/js/components/posts/Index.vue
+++ resources/js/components/posts/Index.vue
@@ -3,28 +3,22 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card card-default">
-                    <div class="card-header">POSTS</div>
-
+                    <div class="card-header">PIPES</div>
+                    
                     <div class="card-body">
-                        <router-link :to="{ name: 'create' }" class="btn btn-md btn-success">TAMBAH POST</router-link>
-
                         <div class="table-responsive mt-2">
                             <table class="table table-hover table-bordered">
                                 <thead>
                                 <tr>
-                                    <th>TITLE</th>
-                                    <th>KONTEN</th>
-                                    <th>AKSI</th>
+                                    <th>id</th>
+                                    <th>code</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <tr v-for="(post, index) in posts" :key="post.id">
-                                    <td>{{ post.title }}</td>
-                                    <td>{{ post.content }}</td>
-                                    <td class="text-center">
-                                        <router-link :to="{name: 'edit', params: { id: post.id }}" class="btn btn-sm btn-primary">EDIT</router-link>
-                                        <button @click.prevent="PostDelete(post.id, index)" class="btn btn-sm btn-danger">HAPUS</button>
-                                    </td>
+                                <tr v-for="(pipe, index) in pipes" :key="pipe.id">
+                                    <td>{{ pipe.id }}</td>
+                                    <td>{{ pipe.code }}</td>
+                                    
                                 </tr>
                                 </tbody>
                             </table>
@@ -42,13 +36,13 @@
     export default {
         data() {
             return {
-                posts: []
+                pipes: []
             }
         },
         created() {
             let uri = `http://localhost:8000/api/data`;
             this.axios.get(uri).then(response => {
-                this.posts = response.data.data;
+                this.pipes = response.data;
             });
         }
     }
